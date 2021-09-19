40448efe81369989e1c09caddd052df9032af283
nobuyukixn <tsundokuxs@gmail.com>
2021-09-19 07:39:45 +0700

    SchaftSMP

---
+++ home.js
@@ -0,0 +1,25 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwzCW1tUWIdwxtIWbutdWwcXKXcOyIMgNc1uHgogXBE9gaH3aVUvF-noX9DhpK40V_Y3g/exec';
+      const form = document.forms['register'];
+      const btnKirim = document.querySelector('.btn-kirim');
+      const btnLoading = document.querySelector('.btn-loading');
+      const myAlert = document.querySelector('.my-alert');
+
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        // ketika tombol submit diklik
+        // tampilkan tombol loading, hilangkan tombol kirim
+        btnLoading.classList.toggle('d-none');
+        btnKirim.classList.toggle('d-none');
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then((response) => {
+            // tampilkan tombol kirim, hilangkan tombol loading
+            btnLoading.classList.toggle('d-none');
+            btnKirim.classList.toggle('d-none');
+            // tampilkan alert
+            myAlert.classList.toggle('d-none');
+            // reset formnya
+            form.reset();
+            console.log('Success!', response);
+          })
+          .catch((error) => console.error('Error!', error.message));
+      });
